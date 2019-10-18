6109e756829088e1b5a70726f7db5a6e6e6f3348
reissgrvs <rwag365@gmail.com>
2019-10-18 22:30:54 +0100

    AlphaBot initial

---
+++ AlphaTeam/Utils.py
@@ -0,0 +1,12 @@
+import math
+
+def getDistance(x1,y1,x2,y2):
+        heading_x = x2-x1
+        heading_y = y2-y1
+        return(math.sqrt((heading_x*heading_x) + (heading_y*heading_y)))
+
+def getAng(x1,y1,x2,y2):
+        t1 = math.atan2(y2-y1,x2-x1)
+        t1 = (t1*180)/math.pi
+        t1 = (t1-360)%360
+        return(360-t1)
