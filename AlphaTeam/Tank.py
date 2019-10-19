31a5066ef379c33f3d302c2031d6f42f965a200e
Scott Wilson <2247638w@student.gla.ac.uk>
2019-10-19 15:09:51 +0100

    Laying groundwork for state system

--- AlphaTeam/Tank.py
+++ AlphaTeam/Tank.py
@@ -14,10 +14,20 @@ class Tank:
     nearest_enemy = 0
     pos = (0,0)
 
+    STATES = ['PATROL','ATTACK', 'GOHEALTH','GOAMMO', 'BANK']  # fill this in as i figure out required states
+    state = 'PATROL'
+
     def __init__(self, ServerDeetz, Team, Name):
         self.name = Team + ":" + Name
+        self.state = 'IDLE'
         self.GameServer = ServerComms(ServerDeetz.hostname, ServerDeetz.port)
+<<<<<<< Updated upstream
         self.GameServer.sendMessage(
+=======
+        # Spawn our tank with starting state
+        # logging.info("Creating tank with name '{}'".format(args.name))
+        self.GameServer.sendMessagei(
+>>>>>>> Stashed changes
             ServerMessageTypes.CREATETANK, {'Name': self.name})
 
     def evalChance(self, player, enemy):
@@ -75,6 +85,12 @@ class Tank:
                 'Amount': getAng(self.pos, point)
             })
 
+    def setState(self, state):
+        if state not in self.STATES:
+            return
+        else:
+            self.state = state
+ 
     def update(self):
         data = self.getInfo()
         self.GameServer.sendMessage(ServerMessageTypes.TOGGLEFORWARD)
@@ -85,15 +101,16 @@ class Tank:
             self.ammo = data["Ammo"]
             self.health = data["Health"]
 
-            if self.pos[1] > 100 or self.pos[1] < -100:
-                self.bank = False
+            if(self.pos[1] > 100 or self.pos[1] < -100):
+                self.setState('PATROL')
 
             if len(data) == 1 and data["Id"] == self.AlphaID:
                 self.turnTo((0, 100))
                 self.target = (0,0)
 
-                if self.nearest_enemy != 0 and self.nearest_enemy["Health"] == 0:
-                    self.bank = True
+               if not(self.nearest_enemy == 0) and self.nearest_enemy["Health"] == 0:
+                    print("HERERERERERERERERERRERERERERERERERER")
+                    self.setState('BANK')
                     self.nearest_enemy["Health"] = 5
 
             if data["Type"] == "Tank" and data["Name"] != self.name:
@@ -104,7 +121,8 @@ class Tank:
                 if data["Type"] == "HealthPickup":
                     self.nearestHPack = (data["X"], data["Y"])
 
-        if self.bank == True:
+        if self.state == 'BANK':
+            self.turnTo(0,100)
             self.goGoals()
         else:
             self.pickTarget()
